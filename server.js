a39798e8fb044b0e0cacbda550fafe33304e900e
soulxmark <mark2899estimada@gmail.com>
2025-03-19 00:26:20 +0800

    changes

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
-//Require - imports
 const express = require('express');
-const bodyParse = require('body-parser');
+const bodyParser = require('body-parser');
 const database = require('./data/database'); 
-const app = express();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json'); // Ensure this file exists
 
+const app = express();
 const port = process.env.PORT || 3001;
-//-----Routes---------
-app.use(bodyParse.json());
+
+// Middleware
+app.use(bodyParser.json());
 
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
@@ -15,13 +17,20 @@ app.use((req, res, next) => {
     next();
 });
 
+// **Add Swagger Route**
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// Register Routes
 app.use("/", require("./routes"));
+
 // Start database connection first
 database.initDb((err) => {
     if (err) {
         console.log(err);
-    } 
-    else {
-        app.listen(port, () => {console.log(`Database connected! Server running on http://localhost:${port}`);});
+    } else {
+        app.listen(port, () => {
+            console.log(`Database connected! Server running on http://localhost:${port}`);
+            console.log(`Swagger Docs available at http://localhost:${port}/api-docs`);
+        });
     }
 });
