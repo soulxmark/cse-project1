d5ba712ba5baaf4d131b922e547db87885c0d2f4
soulxmark <mark2899estimada@gmail.com>
2025-03-28 19:35:49 +0800

    chhanges

--- data/database.js
+++ data/database.js
@@ -7,7 +7,7 @@ let database;
 
 const initDb = (callback) => {
     if(database){
-        console.log('Db  is already initialized1');
+        console.log('Db  is already initialized!    ');
         return callback(null,database);
     }
     MongoClient.connect(process.env.MONGODB_URL)
